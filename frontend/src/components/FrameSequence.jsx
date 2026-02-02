def9491030213ff4e2de818ffe12eea2fb5bf1c1
Aysh Jaiswal <ayushjaiswal452@gmail.com>
2026-02-02 14:51:05 +0530

    g

--- frontend/src/components/FrameSequence.jsx
+++ frontend/src/components/FrameSequence.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
-import Lenis from '@studio-freight/lenis';
+import Lenis from 'lenis';
 import './FrameSequence.css';
 
 const FrameSequence = () => {
